9c3ee7010cce8dedf85f14615e7cc1fbbfe394e6
pascal <pascaldaf2016@gmail.com>
2024-10-08 11:51:52 +0200

    ajout template et interface login admin

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/login', name: 'login')]
+    public function index(): Response
+    {
+        $error = null;
+        $username = null;
+        return $this->render('security/index.html.twig', [
+            'controller_name' => 'SecurityController',
+            'hasError' => $error !== null,
+            'username' => $username,
+        ]);
+    }
+}
