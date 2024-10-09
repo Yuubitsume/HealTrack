1c796179b59f0ebf38e26c67c14da03c1943a9f3
Yuubitsume <jeannicolas34980@gmail.com>
2024-10-09 15:13:06 +0200

    changement css background

--- src/Controller/SoinController.php
+++ src/Controller/SoinController.php
@@ -4,28 +4,19 @@
 
 namespace App\Controller;
 
-use App\Service\DiseaseService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class SoinController extends AbstractController
 {
-    private $diseaseService;
-
-    public function __construct(DiseaseService $diseaseService) // Injection de dépendance
-    {
-        $this->diseaseService = $diseaseService;
-    }
-
     #[Route('/soin', name: 'soin')]
     public function index(): Response
     {
-        $diseases = $this->diseaseService->getDiseases(); // Appel au service
-
+   
         return $this->render('security/soin.html.twig', [
             'controller_name' => 'SoinController',
-            'diseases' => $diseases, // Passez les données à la vue
+        
         ]);
     }
 }
