779d303aac1d2014a559e558c8a57db374b5a527
Yuubitsume <jeannicolas34980@gmail.com>
2024-10-09 09:21:58 +0200

    ajout page soin twig/controller

---
+++ src/Controller/SoinController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class SoinController extends AbstractController
+{
+    #[Route('/soin', name: 'soin')]
+    public function index(): Response
+    {
+        return $this->render('security/soin.html.twig', [
+            'controller_name' => 'SoinController',
+        ]);
+    }
+}
